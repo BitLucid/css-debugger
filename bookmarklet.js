9d98bf425818dd9f5779a290b8c0946afacf86aa
Roy Ronalds <tchalvakspam@gmail.com>
2017-09-22 12:18:43 -0400

    Js and index: Move to https,
    to avoid problems with mixed content errors.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,10 @@
-(function(){
-	var url = 'http://bitlucid.github.io/css-debugger/debugger.css';
-	var link=document.createElement("link");
-	link.setAttribute("rel", "stylesheet");
-	link.setAttribute("type", "text/css");
-	link.setAttribute("href", url);
-	document.getElementsByTagName("head")[0].appendChild(link);
-})();
-alert('Debugging css added to page.');
+(function () {
+    "use strict";
+    var url = 'https://bitlucid.github.io/css-debugger/debugger.css';
+    var link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", url);
+    document.getElementsByTagName("head")[0].appendChild(link);
+}());
+window.alert('Debugging css added to page.');
